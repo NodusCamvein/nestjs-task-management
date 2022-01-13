525e851eadcebe1e4e840eddeea3cc4f1bebbd30
Nodus <NoAbCa@protonmail.com>
2022-01-13 14:03:30 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Task Management
+
+Learned all about NestJS thanks to the following course: https://www.udemy.com/course/nestjs-zero-to-hero/
+
+I installed the NestJS CLI and made use of nest, npm, and yarn commands, although mainly yarn, which made everything smooth. Having a good instructor helped a lot,  and I was able to follow and understand the processes required to build a Task Management Application (REST API) without being too confused at the end...
+
+From the very basics, when we're talking about Modules, Controllers, Providers, and Services, to the use of DTO's, repositories, entities,statuses, imports, exports, directories, types, uuid's, etc.
+
+<!-- Commands used:
+
+yarn global add @nestjs/cli
+nest -v
+nest new nestjs-task-management (chose yarn over npm in this case)
+nest g
+nest g --help
+nest g module tasks
+yarn start:dev
+nest g controller tasks --no-spec
+nest g service tasks --no-spec
+yarn add uuid -->
---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,51 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { Task, TaskStatus } from './task.model';
+import { TasksService } from './tasks.service';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+    if (Object.keys(filterDto).length) {
+      return this.tasksService.getTasksWithFilters(filterDto);
+    } else {
+      return this.tasksService.getAllTasks();
+    }
+  }
+
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id);
+  }
+
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id: string): void {
+    return this.tasksService.deleteTask(id);
+  }
+
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body('status') status: TaskStatus,
+  ): Task {
+    return this.tasksService.updateTaskStatus(id, status);
+  }
+}
